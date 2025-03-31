ecb997aef730d7a5a3b8f464a07d3be926824fc0
James <justjamescreative@outlook.com>
2025-03-30 20:41:08 -0400

    Added a helper comment for the test

--- src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
+++ src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -5,7 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomArrayListTest {
+    // Test-Driven Development Methodology
+    // Step 1 - Write a failing test
+    // Step 2 - Write the business logic to make the test pass
+    // Step 3 - Refactor your code
+    // Rinse and repeat Steps 1 - 3
 
+    // Three A's - "ARRANGE, ACT, ASSERT"
+    // Arrange - set things up
+    // Act - calling the method
+    // Assert - expected behavior/actual behavior
     @Test
     void add() {
     }
