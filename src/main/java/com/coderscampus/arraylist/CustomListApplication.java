b16aa08d7817a36808d28cc87719181626d9813b
James <justjamescreative@outlook.com>
2025-04-06 09:01:48 -0400

    Added runnable code to the Application class

--- src/main/java/com/coderscampus/arraylist/CustomListApplication.java
+++ src/main/java/com/coderscampus/arraylist/CustomListApplication.java
@@ -1,7 +1,34 @@
 package com.coderscampus.arraylist;
 
+import com.coderscampus.arraylist.domain.CustomArrayList;
+
 public class CustomListApplication {
     public static void main(String[] args) {
 
+        CustomList<Integer> numbers = new CustomArrayList<>();
+
+        for (int i = 0; i < 25; i++) {
+            numbers.add(i + 1);
+        }
+
+        for (int i = 0; i < numbers.getSize(); i++) {
+            System.out.println(numbers.get(i) + " ");
+        }
+
+        numbers.add(10, 33);
+        for (int i = 0; i < numbers.getSize(); i++) {
+            System.out.println(numbers.get(i) + " ");
+        }
+
+        numbers.remove(3);
+        for (int i = 0; i < numbers.getSize(); i++) {
+            System.out.println(numbers.get(i) + " ");
+        }
+
+        try {
+            System.out.println(numbers.get(10000));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
