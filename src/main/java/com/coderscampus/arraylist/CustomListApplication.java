f5669ea9a7b42ed7250c4f270d5c26c508aa4299
James <justjamescreative@outlook.com>
2025-04-06 09:03:28 -0400

    Added some spacing for the runnable

--- src/main/java/com/coderscampus/arraylist/CustomListApplication.java
+++ src/main/java/com/coderscampus/arraylist/CustomListApplication.java
@@ -10,21 +10,20 @@ public class CustomListApplication {
         for (int i = 0; i < 25; i++) {
             numbers.add(i + 1);
         }
-
         for (int i = 0; i < numbers.getSize(); i++) {
             System.out.println(numbers.get(i) + " ");
         }
-
+        System.out.println();
         numbers.add(10, 33);
         for (int i = 0; i < numbers.getSize(); i++) {
             System.out.println(numbers.get(i) + " ");
         }
-
+        System.out.println();
         numbers.remove(3);
         for (int i = 0; i < numbers.getSize(); i++) {
             System.out.println(numbers.get(i) + " ");
         }
-
+        System.out.println();
         try {
             System.out.println(numbers.get(10000));
         } catch (IndexOutOfBoundsException e) {
